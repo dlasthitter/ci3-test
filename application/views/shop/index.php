e49deae50942242dfba893de4cfd8a624c3fc3e8
Monico <monico@newunion.sg>
2017-08-04 17:46:19 +0800

    Update

--- application/views/shop/index.php
+++ application/views/shop/index.php
@@ -7,16 +7,18 @@
     </thead>
     <tbody>
 	<?php
-		if (!$items->num_rows){
+		if (!$items->result_id->num_rows){
 				echo "<tr><td colspan='99'>No items found.</td></tr>";
 		}
 		else{
 			foreach ($items->result() as $key => $val):
 	?>
-				<tr class="<?=$class?>">
+				<tr>
 	          		<td></td>
 	          		<td>
-	          			<?= $val->name ?>
+	          			<?= $val->name ?> <?= $val->price ?><br />
+	          			<input type="text" name="qty[<?= $val->item_id ?>]"><br />
+	          			<a href="#" class="btn btn-sm btn-primary">Add to cart</a>
 	          		</td>
           		</tr>
     <?php	
